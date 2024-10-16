f6da4fa0fddbbc2c0f95194e86f2539483a35e4c
aminulhoque786 <aminulsara6@gmail.com>
2024-10-16 20:41:05 +0600

    done

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,85 +1,99 @@
-import React, { useState, useRef } from "react";
-
-import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
-import Post from "../components/Post";
+import React, { useContext, useState } from "react";
 import Container from "../components/Container";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+import Post from "../components/Post";
+import { ApiData } from "../components/ContextApi";
+import Pagination from "../components/Pagination";
 
 const Shop = () => {
-  let [show, setshow] = useState(false);
+  let {info, loading} = useContext(ApiData)
+  let [show, setShow] = useState(false);
+  let [currentPage, setCurrentPage] = useState(1)
+  let [perPage, setPerPage] = useState(6)
+
+  let lastPage = currentPage * perPage
+  let firstPage = lastPage - perPage
+  let allPage = info.slice(firstPage, lastPage)
+
+  let pageNumber = []
+
+  for(let i = 0; i < Math.ceil(info.length / perPage); i++){
+    pageNumber.push(i)
+  }
+
+  console.log(pageNumber);
+  
+  
+  
+
 
   return (
     <section>
       <Container>
         <div className="flex">
           <div className="w-1/5">
-            <div className="mt-8">
+            <div className="pr-6 pt-8">
               <div
-                className="flex justify-between  "
-                onClick={() => setshow(!show)}
+                className="flex justify-between items-center"
+                onClick={() => setShow(!show)}
               >
-                <h2 className="text-[#262626] font-bold text-[20px] font-sans  ">
-                  Shop by category
+                <h2 className="text-[#262626] font-bold text-[20px] font-sans">
+                  Shop by Category
                 </h2>
-                {show ? <FaMinusCircle /> : <FaPlusCircle />}
+
+                {show ? <FaMinus /> : <FaPlus />}
               </div>
               {show && (
-                <ul className="">
-                  <li className="font-sans text-[14px] text-black  pl-3 pt-3 border-b-[1px] border-blue-600 font-semibold hover:pl-6 duration-300 ease-in-out">
-                    category-1
-                  </li>
-                  <li className="font-sans text-[14px] text-black  pl-3 pt-3  border-b-[1px] border-blue-600 font-semibold hover:pl-6 duration-300 ease-in-out">
-                    category-2
-                  </li>
-
-                  <li className="font-sans text-[14px] text-black  pl-3 pt-3  border-b-[1px] border-blue-600 font-semibold hover:pl-6 duration-300 ease-in-out">
-                    category-4
-                  </li>
-                  <li className="font-sans text-[14px] text-black  pl-3 pt-3  hover:pl-6 font-semibold  duration-300 ease-in-out">
-                    category-3
-                  </li>
+                <ul>
+                  <li>Categroy</li>
+                  <li>Categroy</li>
+                  <li>Categroy</li>
+                  <li>Categroy</li>
+                  <li>Categroy</li>
                 </ul>
               )}
             </div>
           </div>
           <div className="w-4/5">
-            <div className="pt-8 flex justify-end items-center gap-x-4">
+            <div className="pt-8 flex justify-end items-center gap-x-10">
               <div className="">
-                <label className="pr-3 font-semibold" htmlFor="">
+                <label className="pr-3" htmlFor="">
                   Sort By :
                 </label>
                 <select
-                  classNamE="W-[60px] h-[40px] border-[1px] font-bold border-black "
+                  className="w-[60px] h-[30px] border-[1px] border-[#262626]"
+                  name=""
                   id=""
                 >
                   <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
+                  <option value="">Two</option>
+                  <option value="">Three</option>
+                  <option value="">Four</option>
                 </select>
               </div>
               <div className="">
-                <label className="pr-3 font-semibold" htmlFor="">
-                  Show :
+                <label className="pr-3" htmlFor="">
+                  Show:
                 </label>
                 <select
-                  classNamE="W-[60px] h-[40px] border-[1px] border-black"
+                  className="w-[60px] h-[30px] border-[1px] border-[#262626]"
+                  name=""
                   id=""
                 >
                   <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
+                  <option value="">Two</option>
+                  <option value="">Three</option>
+                  <option value="">Four</option>
                 </select>
               </div>
-              <div className=""></div>
             </div>
-          </div>
-        </div>
-        <div className="flex justify-end">
-          <div className="w-4/5 flex ">
-            <Post />
+
+            <div className="flex justify-between flex-wrap">
+              <Post allPage={allPage} />
+              <div className="py-10 flex justify-center w-full">
+              <Pagination pageNumber={pageNumber}/>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
